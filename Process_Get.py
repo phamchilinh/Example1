4eef073ee1743b4ac7599fef5a624a269c476e71
= <=>
2021-07-11 10:23:59 +0700

    Linh update file thread, process

--- Process_Get.py
+++ Process_Get.py
@@ -5,20 +5,25 @@ from multiprocessing import Process, Manager
 
 def get_url(url, dict):
     start_time = time.time()
-    object = requests.get(url)
+    for r in url:
+      object = requests.get(r)
 
-    dict[url] = time.time() - start_time
-
+    dict[url[0]] = time.time() - start_time
 
 if __name__ == "__main__":
     URLS = ['https://docs.python.org/', 'https://realpython.com/', 'https://kaggle.com/', 'https://github.com/',
             'https://www.coursera.org/', 'https://trello.com/', 'https://www.sciencedirect.com/',
             'https://www.youtube.com/', 'https://bachasoftware.com/']
     with Manager() as mn:
+
+        l0 = mn.list(URLS[:3])
+        l1 = mn.list(URLS[3:6])
+        l2 = mn.list(URLS[6:9])
+
         start_time_total = time.time()
         dict = mn.dict()
         try:
-            process = [Process(target=get_url, args=[url, dict, ]) for url in URLS]
+            process = [Process(target=get_url, args=[url, dict, ]) for url in [l0, l1, l2]]
             for p in process:
                 p.start()
             for p in process:
--- Thread_Get.py
+++ Thread_Get.py
@@ -3,31 +3,37 @@ import time
 from multiprocessing import Manager
 import threading
 
-
 def get_url(url, dict):
     start_time = time.time()
-    object = requests.get(url)
-    dict[url] = time.time() - start_time
+    for r in url:
+      object = requests.get(r)
+    dict[url[0]] =  time.time() - start_time
 
 
 if __name__ == "__main__":
     URLS = ['https://docs.python.org/', 'https://realpython.com/', 'https://kaggle.com/', 'https://github.com/',
             'https://www.coursera.org/', 'https://trello.com/', 'https://www.sciencedirect.com/',
             'https://www.youtube.com/', 'https://bachasoftware.com/']
+    start_time_total = time.time()
+
     with Manager() as mn:
-        start_time_total = time.time()
+        l0 = mn.list(URLS[:3])
+        l1 = mn.list(URLS[3:6])
+        l2 = mn.list(URLS[6:9])
         dict = mn.dict()
         try:
             thread = []
-            for url in URLS:
-              t = threading.Thread(target=get_url, args=(url, dict))
-              thread.append(t)
+            t1 = threading.Thread(target=get_url, args=[l0, dict])
+            thread.append(t1)
+            t2 = threading.Thread(target=get_url, args=[l1, dict])
+            thread.append(t2)
+            t3 = threading.Thread(target=get_url, args=[l2, dict])
+            thread.append(t3)
+            # thread = [Thread(target=get_url, args=(url, dict, )) for url in URLS]
             for t in thread:
               t.start()
             for t in thread:
               t.join()
-              if t.isAlive():
-                t.setDaemon(True)
             time_total = time.time() - start_time_total
             print("Total of thread: ", time_total)
             print("List time in url : ", dict)
